38c994bf7b6c1a3831d20d8a55249127a77a29a6
Eugene Lim <euracoze@gmail.com>
2014-10-01 21:31:51 +0800

    added LinearSearch

---
+++ algorithms/LinearSearch.java
@@ -0,0 +1,30 @@
+package algorithms;
+
+public class LinearSearch<T> {
+
+  public int search(T[] items, T key) {
+
+    for (int searchIndex = 0; searchIndex < items.length; searchIndex++) {
+
+      if (key.equals(items[searchIndex])) {
+
+        return searchIndex;
+
+      }
+
+    }
+
+    return -1;
+
+  }
+
+  public static void main(String[] args) {
+
+    LinearSearch<String> ls = new LinearSearch<String>();
+    String[] fruitNames = { "apple", "grape", "orange", "banana", "mango"};
+    String key = "banana";
+    System.out.format("Key %s at %d.\n", key, ls.search(fruitNames, key));
+
+  }
+
+}
